d10bc0a85374e3581b59f60f0fddfcddfb2f817f
Alish Chapagain <alishchapagain2580@gmail.com>
2026-01-01 22:02:43 +0545

    Python Basics

--- Phase 1 - Python/Basics/list.py
+++ Phase 1 - Python/Basics/list.py
@@ -1,2 +1,24 @@
-lst = [1,2,3,4,5,6]
+lst = [1,4,6,2,7,3]
+lst1 = [11,12,13]
+print(lst)
+
+print(lst[3])
+print(lst[1:4])
+
+#List Methods
+# 1 Append
+lst.append(8)
+print(lst)
+
+# 2 Sort
+lst.sort() #Ascending
+print(lst)
+#lst.sort(reverse=True) #Descending
+
+# insert the list
+lst.insert(4,5) # index, element
+print(lst)
+
+#add two list
+lst.extend(lst1)
 print(lst)
