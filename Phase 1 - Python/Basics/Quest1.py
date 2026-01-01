a718569d3c90e1bc9fcd058852b9e6be477a79ea
Alish Chapagain <alishchapagain2580@gmail.com>
2026-01-01 22:35:18 +0545

    Python Basics

---
+++ Phase 1 - Python/Basics/Quest1.py
@@ -0,0 +1,11 @@
+#WAP to ask the user to enter names of their favourire 3 movies & store them in a list
+
+movies = []
+mov = input("Enter your 1st Fav Movie: ")
+movies.append(mov)
+mov = input("Enter your 2nd Fav Movie: ")
+movies.append(mov)
+mov = input("Enter your 3rd Fav Movie: ")
+movies.append(mov)
+
+print(movies)
---
+++ Phase 1 - Python/Basics/tuple.py
@@ -0,0 +1,12 @@
+#NOTE: Tuple can't assignment - cannot change its value later
+tup=(87,99,50,76,45,76)
+
+print(tup)
+print(tup[3])
+
+print(type(tup))
+print(tup.index(45)) # Index number of element 45
+print(tup.count(76)) # Total no. of 76 in tup
+
+# tup[5]=30 (Not Allowed)
+
