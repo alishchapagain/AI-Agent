5208e5e52eba59d3540ded00718eaffba7636b79
Alish Chapagain <alishchapagain2580@gmail.com>
2026-01-02 21:00:59 +0545

    Function Basics

---
+++ Phase 1 - Python/Basics/functions.py
@@ -0,0 +1,10 @@
+#Function to claculate Sum
+
+#Function Define
+def calSum(a,b): #Parameters
+    sum=a+b
+    return sum
+
+a=int (input("Enter 1st Number: "))
+b=int (input("Enter 2nd Number: "))
+print((calSum(a,b))) #Function call
